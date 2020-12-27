76c7f168de183b0c8de802e89da2c30d2bdda81e
Lee Cooper <master6894@gmail.com>
2020-12-27 23:17:53 +0300

    Update status and project structure

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Cpp-discipline
 
-Cpp-discipline is an attempt to learn C ++. 
+Cpp-discipline is an attempt to learn C++. 
 This code is an implementation of training tasks. 
 
 ## Current status
 - [x] Block 1: 52 / 52 completed.
-- [ ] Block 2: 19 completed.
+- [ ] Block 2: 22 completed.
 - [ ] Block 3.
 - [ ] Block 4.
 - [ ] Block 5.
---
+++ block-2/yandex/04-count-repetitions-yandex.cpp
@@ -0,0 +1,24 @@
+/* 
+Внимание! Ниже опубликовано решение необязательной задачи «База регионов». 
+Настоятельно рекомендуем попытаться её решить перед тем, как читать решение.
+*/
+
+#include <map>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+bool operator<(const Region& lhs, const Region& rhs) {
+    return tie(lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
+            tie(rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
+}
+
+int FindMaxRepetitionCount(const vector<Region>& regions) {
+    int result = 0;
+    map<Region, int> repetition_count;
+    for (const Region& region : regions) {
+        result = max(result, ++repetition_count[region]);
+    }
+    return result;
+}
